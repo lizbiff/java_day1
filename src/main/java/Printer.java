0c88a5abb1e6503f8188d60b05e98f4d1f890eff
Sarah Grant <sarah@ghijk.co.uk>
2019-11-05 10:09:16 +0000

    started to work  on printer file

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,21 @@
 public class Printer {
+
+    private int paper;
+    private int toner;
+
+    public Printer(int paper, int toner){
+        this.paper = paper;
+        this.toner = toner;
+    }
+
+    public void print(int pages, int copies) {
+        if ((pages * copies) <= numberOfCopies){
+            this.paper -= (pages * copies);
+            this.toner -= (pages * copies);
+        }
+    }
+
+    public int getSheetsLeft(){
+        return this.paper;
+    }
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
 public class PrinterTest {
+
+Printer printer;
+
+@Before
+    public void setUp() {
+    printer = new Printer(100, 100);
+}
+
+@Test
+    public void printerReduction(){
+    printer.print(5, 10);
+    assertEquals(50, printer.getSheetsLeft());
+}
+
 }
